55fce4834947fc9c7e1b9599a0a7e4b00279b248
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2012-06-07 11:27:34 -0500

    Vital fix for as-yet unused debug function.
    Allowed pointer option to be set to "parent" or "all" (default; same as true) to specify whether just the parent element's or its and all its children's pointers should be set to cursor.  This works in Firefox and Chrome but IE has an issue and I have employed an odd workaround that I have not thoroughly tested.
    
    Tried to facilitate multiple simultaneous text selection and if successful would have noted this among other things: Made it so when there's a child of a parent that's an input, it's text gets selected, too.  Added an option for this called select with settings "all" or "text" with all selecting the input including text and text just leaving the inputs be.  Also, a target can be more than one element now, etc.

--- jquery.selectify.js
+++ jquery.selectify.js
@@ -1,5 +1,5 @@
 /*!
- * Selectify v1.0.0
+ * Selectify v1.0.1
  * http://github.com/terrasoftlabs/jquery-selectify
  *
  * Copyright © 2012 Gabriel Nahmias.
@@ -8,8 +8,36 @@
  *
  */
 
-// TODO: Make global defaults possible.
-//		 WHEN SELECTIFYING THE BODY THROUGH TARGET, TOGGLE HAS NO BEARING.  Fix.
+/* TODO:
+ *		 
+ *		 Make global defaults possible.
+ *		 
+ *		 Make prototype or whatever editable like above except it has more to do with
+ *		 editing an already existing Selectify call through chaining, etc.  For example,
+ *		 with the child-test examples on index.html, I could modify the settings for the
+ *		 call on another element through that chain instead of calling it separately with
+ *		 :eq kind of logic.
+ *		 
+ *		 Can't have both text input and text on page selected at same time??  Interesting.
+ *		 
+ *		 Add option to change title depending on whether selected or not.
+ *		 
+ *		 Change association of data() to element itself?  Not sure.
+ *		 
+ *		 WHEN SELECTIFYING THE BODY THROUGH TARGET, TOGGLE HAS NO BEARING.  Fixed?
+ *
+ * Known issues:
+ *		 
+ *		 Chome:
+ *		 
+ *		 	Selectifying with a target with toggle enabled doesn't work... hmm.
+ *		 	will fix soon.
+ *		 
+ *		 IE:
+ *		 
+ *		 	... there's some but FUCK IE.
+ *		 
+ */
 
 ( function($) {
 	
@@ -25,6 +53,7 @@
 		// Variables.
 		
 		var that = this,				// Copy current object for later use.
+			bIE = $.browser.msie,		// Using IE?
 			bSelected = false,			// Acts like a more accurate .is(":focus").
 			oDoc = document,
 			sMsg,
@@ -35,56 +64,13 @@
 			debug:		false,
 			pointer:	true,
 			target:		null,
-			title:		"Click to select code",
+			//select:		"all",
+			title:		"Click to select text",
 			toggle:		false
 			
 		}, oParams);
 		
 		var it = this.oSettings.target;
-		
-		if ( $(it).length <= 0 && typeof it != 'undefined' && it != null )	
-			throw new Error(NAME + ': Invalid target specified with selector "' + it + '" (attached to element selected by ' + this.selector + ').');
-		
-		if (this.oSettings.pointer)
-			this.css("cursor", "pointer");
-		
-		/* Debugging output */
-		
-		if (this.oSettings.debug) {
-			
-			sMsg = "Element selectified:";
-			
-			if ($.browser.msie) {
-				
-				console.log(sMsg, this);
-				
-			} else {
-				
-				console.debug(sMsg, this);
-				console.debug(" ");
-				
-			}
-			
-			if (typeof it != 'undefined' && it != null) {
-				
-				sMsg = "Its target is:";
-				
-				if ($.browser.msie) {
-					
-					console.log(sMsg, it);
-					
-				} else {
-					
-					console.debug(sMsg, it);
-					console.debug(" ");
-					
-				}
-				
-			}
-			
-		}
-		
-		/* End main debugging output */
 		
 		function debug() {
 			
@@ -92,34 +78,81 @@
 			
 			for (var i = 0; i < arguments.length; i++) {
 				
-				console.debug( arguments[0] );
+				console.debug( arguments[i] );
 				
 			}
 			
 		}
 		
-		function selectText(element, event) {
+		// Begin selectText().
+		
+		function selectText(element/*, event*/) {
 			
-			var oEl = element[0];
+			// NOTE:	I've edited this function quite a bit especially in how it returns.  Previously,
+			//			I believe it did not return anything, merely it applied some things to the selected
+			//			elements.  Not sure if this is good.  Investigate.  What I'm trying to do is allow
+			//			Multiple selections to be made at once but to no avail.  Oh, well!
 			
-			// Need to work in if the element is currently selected or something so all selections
-			// sync up.  It varies from input to text... this is fine for now.
+			var iElements = element.length;
 			
-			bSelected = that.data(sSelected); //( window.getSelection().toString() != "" );
-			
-			//var sType = oEl.nodeName.toLowerCase();
-			
-			if ( element.is("input") ) {
+			return element.each( function(i, v) {
 				
-				// It's an input field so there's a totally different way to select the text.
+				var oCurrent = element[i];
 				
-				if (that.oSettings.toggle) {
+				var $Current = $(oCurrent);
+				
+				// Need to work in if the element is currently selected or something so all selections
+				// sync up.  It varies from input to text... this is fine for now.
+				
+				bSelected = that.data(sSelected); //( window.getSelection().toString() != "" );
+				
+				//var sType = oCurrent.nodeName.toLowerCase();
+				
+				if ( $Current.is("input") ) {
 					
-					if (!bSelected) {
+					// It's an input field so there's a totally different way to select the text.
+					
+					if (that.oSettings.toggle) {
+						
+						if (!bSelected) {
+							
+							if (that.oSettings.debug) {
+								
+								if (bIE)
+									console.log(DEBUG_SELECTED);
+								else
+									console.debug(DEBUG_SELECTED);
+								
+							}
+							
+							that.data(sSelected, true);
+							
+							$Current.select();
+							
+						} else {
+							
+							if (that.oSettings.debug) {
+								
+								if (bIE)
+									console.log(DEBUG_DESELECTED);
+								else
+									console.debug(DEBUG_DESELECTED);
+								
+							}
+							
+							that.data(sSelected, false);
+							
+							$Current.blur();
+							
+						}
+						
+					} else {
+						
+						// No toggle.
 						
 						if (that.oSettings.debug) {
 							
-							if ($.browser.msie)
+							if (bIE)
 								console.log(DEBUG_SELECTED);
 							else
 								console.debug(DEBUG_SELECTED);
@@ -128,135 +161,255 @@
 						
 						that.data(sSelected, true);
 						
-						element.select();
-						
-					} else {
-						
-						if (that.oSettings.debug) {
-							
-							if ($.browser.msie)
-								console.log(DEBUG_DESELECTED);
-							else
-								console.debug(DEBUG_DESELECTED);
-							
-						}
-						
-						that.data(sSelected, false);
-						
-						element.blur();
+						$Current.select();
 						
 					}
 					
 				} else {
 					
-					// No toggle.
-					
-					if (that.oSettings.debug) {
+					if (oDoc.body.createTextRange) {
 						
-						if ($.browser.msie)
-							console.log(DEBUG_SELECTED);
-						else
-							console.debug(DEBUG_SELECTED);
+						// IE < 9
+						
+						var oRangeOld = oDoc.body.createTextRange(),
+							oSelection = oDoc.selection;
+						
+						if (that.oSettings.toggle && bSelected) {
+							
+							if (that.oSettings.debug)
+								console.log(DEBUG_DESELECTED);
+							
+							that.data(sSelected, false);
+							
+							// I've got to think of a better way to do this if there is one.
+							
+							//oSelection.removeAllRanges();
+							
+							oSelection.empty();
+							
+						} else {
+							
+							if (that.oSettings.debug)
+								console.log(DEBUG_SELECTED);
+							
+							that.data(sSelected, true);
+							
+							oRangeOld.moveToElementText(oCurrent);
+							
+							oRangeOld.select();
+							
+						}
+						
+					} else if (window.getSelection) {
+						
+						// Mozilla, Opera, WebKit, IE9, etc.					
+						
+						var oRange = oDoc.createRange(),
+							oSelection = window.getSelection();
+						
+						if (that.oSettings.toggle && bSelected) {
+							
+							if (that.oSettings.debug) {
+								
+								if (bIE)
+									console.log(DEBUG_DESELECTED);
+								else
+									console.debug(DEBUG_DESELECTED);
+								
+							}
+							
+							that.data(sSelected, false);
+							
+							oSelection.removeRange(oRange);
+							
+						} else {
+							
+							if (that.oSettings.debug) {
+								
+								if (bIE)
+									console.log(DEBUG_SELECTED);
+								else
+									console.debug(DEBUG_SELECTED);
+								
+							}
+							
+							that.data(sSelected, true);
+							
+							oRange.selectNodeContents(oCurrent);
+							
+							oSelection.removeAllRanges();
+							
+							oSelection.addRange(oRange);
+							
+						}
+						
+						if (that.oSettings.debug) {
+							
+							console.debug("Range object: ", oRange);
+							console.debug("Selection object: ", oSelection);
+							
+						}
 						
 					}
+				
+				}
+				
+			} );
+			
+			// event.preventDefault();
+			
+		}
+		
+		// End selectText().
+		
+		// BEGIN MAIN EXECUTION AREA
+		
+		if ( $(it).length <= 0 && typeof it != 'undefined' && it != null )	
+			throw new Error(NAME + ': Invalid target specified with selector "' + it + '" (attached to element selected by ' + this.selector + ').');
+		
+		// Begin debugging output.
+		
+		if (this.oSettings.debug) {
+			
+			sMsg = "Element selectified:";
+			
+			if (bIE) {
+				
+				console.log(sMsg, this);
+				
+			} else {
+				
+				console.debug( sMsg, $(this.selector) );
+				
+			}
+			
+			if (typeof it != 'undefined' && it != null) {
+				
+				sMsg = "Its target is:";
+				
+				if (bIE) {
 					
-					that.data(sSelected, true);
+					console.log(sMsg, it);
 					
-					element.select();
+				} else {
+					
+					console.debug(sMsg, it);
+					
+				}
+				
+			}
+			
+		}
+		
+		// End main debugging output.
+		
+		// Pointer handling.
+		
+		if (this.oSettings.pointer) {
+			
+			var iKids = this.children().length,
+				mPointer = this.oSettings.pointer,
+				sProp = "cursor",
+				sVal = "pointer";
+			
+			sMsg = "Changing cursor to pointer.";
+			
+			if (mPointer == "all" || mPointer == true) {
+				
+				// If pointer is set to all or true (and not parent), apply the pointer to
+				// everything including its kids.
+				
+				if (bIE) {
+					
+					console.log(sMsg);
+					
+				} else {
+					
+					console.debug(sMsg);
+					
+				}
+				
+				this.css(sProp, sVal)
+				
+				// If it's got kids, add the title to them, too.
+				
+				sMsg = "Changing cursor to pointer for children:";
+				
+				if (bIE) {
+					
+					console.log(sMsg);
+					
+				} else {
+					
+					console.debug(sMsg);
+					
+				}
+				
+				if (iKids > 0) {
+					
+					this.children().each( function() {
+						
+						sMsg = "Child: ";
+						
+						if (bIE) {
+							
+							console.log(sMsg, this);
+							
+						} else {
+							
+							console.debug(sMsg, this);
+							
+						}
+						
+						$(this).css(sProp, sVal);
+						
+					} );
+					
+				}
+				
+			} else if (mPointer == "parent") {
+				
+				// If pointer is set to parent, apply the pointer to just the parent
+				// element excluding kids.
+				
+				if (bIE) {
+					
+					console.log(sMsg);
+					
+				} else {
+					
+					console.debug(sMsg);
+					
+				}
+				
+				this.css(sProp, sVal);
+				
+				// Workaround for IE.
+				
+				if (bIE) {
+					
+					if (iKids > 0) {
+						
+						this.children().each( function() {
+							
+							sMsg = "Removing cursor for child (workaround for IE): ";
+							
+							console.log(sMsg, this);
+							
+							$(this).css(sProp, "auto");
+							
+						} );
+						
+					}
 					
 				}
 				
 			} else {
 				
-				if (oDoc.body.createTextRange) {
-					
-					// IE < 9
-					
-					var oRangeOld = oDoc.body.createTextRange(),
-						oSelection = oDoc.selection;
-					
-					if (that.oSettings.toggle && bSelected) {
-						
-						if (that.oSettings.debug)
-							console.log(DEBUG_DESELECTED);
-						
-						that.data(sSelected, false);
-						
-						// I've got to think of a better way to do this if there is one.
-						
-						//oSelection.removeAllRanges();
-						
-						oSelection.empty();
-						
-					} else {
-						
-						if (that.oSettings.debug)
-							console.log(DEBUG_SELECTED);
-						
-						that.data(sSelected, true);
-						
-						oRangeOld.moveToElementText(oEl);
-						
-						oRangeOld.select();
-						
-					}
-					
-				} else if (window.getSelection) {
-					
-					// Mozilla, Opera, WebKit, IE9, etc.					
-					
-					var oRange = oDoc.createRange(),
-						oSelection = window.getSelection();
-					
-					if ( that.oSettings.toggle && bSelected ) {
-						
-						if (that.oSettings.debug) {
-							
-							if ($.browser.msie)
-								console.log(DEBUG_DESELECTED);
-							else
-								console.debug(DEBUG_DESELECTED);
-							
-						}
-						
-						that.data(sSelected, false);
-						
-						oSelection.removeRange(oRange);
-						
-					} else {
-						
-						if (that.oSettings.debug) {
-							
-							if ($.browser.msie)
-								console.log(DEBUG_SELECTED);
-							else
-								console.debug(DEBUG_SELECTED);
-							
-						}
-						
-						that.data(sSelected, true);
-						
-						oRange.selectNodeContents(oEl);
-						
-						oSelection.removeAllRanges();
-						
-						oSelection.addRange(oRange);
-						
-					}
-					
-					if (that.oSettings.debug) {
-						
-						console.debug(" ");
-						console.debug("Range object: ", oRange);
-						console.debug("Selection object: ", oSelection);
-						
-					}
-					
-				}
-			
+				// It's not parent, all, or true... so probably false or something related.
+				// Do nothing.
+				
 			}
-			
-			// event.preventDefault();
 			
 		}
 		
@@ -270,7 +423,7 @@
 					
 					sMsg = "Adding title.";
 					
-					if ($.browser.msie)
+					if (bIE)
 						console.log(sMsg);
 					else
 						console.debug(sMsg);
@@ -297,7 +450,7 @@
 				
 				sMsg = "Clicked.";
 				
-				if ($.browser.msie)
+				if (bIE)
 					console.log(sMsg);
 				else
 					console.debug(sMsg);
@@ -315,14 +468,17 @@
 				// Not sure if commenting the conditional has any lasting effects.
 				
 				// if ( $this.hasClass(CLASS) )
-					selectText($Target, event);
+					selectText($Target);
 				
-			} else
-				selectText($this, event);
+			} else {
+				
+				selectText($this);
+				
+			}
 			
 		} );
 		
-		return this;
+		// END MAIN EXECUTION AREA.
 		
 	}
 
